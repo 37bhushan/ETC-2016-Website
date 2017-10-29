4aed16196e825385e890ad5b2551d3ce55192680
37bhushan <37bhushan@gmail.com>
2017-10-29 08:26:15 +0530

    Initial Commit

---
+++ ETC/Assest/Install Mac OS X Yosemite_El Capitan on VMware Workstation 11 inside Windows » Macdrug_files/rate(2).php
@@ -0,0 +1,6 @@
+PDRTJS_8041021_comm_744.nero_up = 0;
+PDRTJS_8041021_comm_744.nero_dn = 0;		
+PDRTJS_settings_8041021_comm_744= { "type" : "nero" , "size" : "sml" , "star_color" : "hand" , "custom_star" : "" , "font_size" : "" , "font_line_height" : "16px" , "font_color" : "" , "font_align" : "left" , "font_position" : "right" , "font_family" : "" , "font_bold" : "normal" , "font_italic" : "normal" , "text_vote" : "Vote" , "text_votes" : "Votes" , "text_rate_this" : "Rate This" , "text_1_star" : "Very Poor" , "text_2_star" : "Poor" , "text_3_star" : "Average" , "text_4_star" : "Good" , "text_5_star" : "Excellent" , "text_thank_you" : "Thank You" , "text_rate_up" : "Rate Up" , "text_rate_down" : "Rate Down"  };
+PDRTJS_8041021_comm_744.init();		
+PDRTJS_8041021_comm_744.token='b2469dfca8d7d24780f04634e0011e6f';
+/*8041021,_comm_744,wp-comment-744,1698275636,0-0*/
---
+++ ETC/Assest/Install Mac OS X Yosemite_El Capitan on VMware Workstation 11 inside Windows » Macdrug_files/rate.php
@@ -0,0 +1,6 @@
+PDRTJS_7972418_post_3331.avg_rating = 4;
+PDRTJS_7972418_post_3331.votes = 12;		
+PDRTJS_settings_7972418_post_3331= { "type" : "stars" , "size" : "sml" , "star_color" : "yellow" , "custom_star" : "" , "font_size" : "" , "font_line_height" : "16px" , "font_color" : "#0" , "font_align" : "left" , "font_position" : "right" , "font_family" : "" , "font_bold" : "normal" , "font_italic" : "normal" , "text_vote" : "Vote" , "text_votes" : "Votes" , "text_rate_this" : "Rate This" , "text_1_star" : "Very Poor" , "text_2_star" : "Poor" , "text_3_star" : "Average" , "text_4_star" : "Good" , "text_5_star" : "Excellent" , "text_thank_you" : "Thank You" , "text_rate_up" : "Rate Up" , "text_rate_down" : "Rate Down"  };
+PDRTJS_7972418_post_3331.init();		
+PDRTJS_7972418_post_3331.token='95fb44cfff0995c7718549d074b64506';
+/*7972418,_post_3331,wp-post-3331,1698275636,4-12*/
