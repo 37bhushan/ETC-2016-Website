55b72862a4670352c61540e296235338caea86d4
37bhushan <37bhushan@gmail.com>
2017-10-29 08:27:04 +0530

    Deleted Unnecessary files

--- ETC/Assest/Install Mac OS X Yosemite_El Capitan on VMware Workstation 11 inside Windows » Macdrug_files/rate(3).php
+++
@@ -1,6 +0,0 @@
-PDRTJS_8041021_comm_745.nero_up = 0;
-PDRTJS_8041021_comm_745.nero_dn = 0;		
-PDRTJS_settings_8041021_comm_745= { "type" : "nero" , "size" : "sml" , "star_color" : "hand" , "custom_star" : "" , "font_size" : "" , "font_line_height" : "16px" , "font_color" : "" , "font_align" : "left" , "font_position" : "right" , "font_family" : "" , "font_bold" : "normal" , "font_italic" : "normal" , "text_vote" : "Vote" , "text_votes" : "Votes" , "text_rate_this" : "Rate This" , "text_1_star" : "Very Poor" , "text_2_star" : "Poor" , "text_3_star" : "Average" , "text_4_star" : "Good" , "text_5_star" : "Excellent" , "text_thank_you" : "Thank You" , "text_rate_up" : "Rate Up" , "text_rate_down" : "Rate Down"  };
-PDRTJS_8041021_comm_745.init();		
-PDRTJS_8041021_comm_745.token='5073e31cad9cf1eec960253ce1dd3106';
-/*8041021,_comm_745,wp-comment-745,1698275636,0-0*/
